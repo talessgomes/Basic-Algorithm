af00b2266737ca5fa9943ae3ee7af423d34686f6
Tales Gomes <tales.santos.gomes@outlook.com>
2022-12-07 09:18:32 -0300

    finalizado-exercicio-10

---
+++ ex10/index.js
@@ -0,0 +1,15 @@
+function booWho(bool) {
+
+    if (bool === true || bool === false) {
+        return true;
+    } else {
+        return false
+    }
+}
+
+booWho(null);
+
+
+/*Este desafio é para saber se o que está sendo passado é um boolean
+ou não */
+
